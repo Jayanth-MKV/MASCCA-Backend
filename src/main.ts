157fe22ca6ab4001f993ec64a70f909cec5788d1
Jayanth MKV <mkvjayanth1@gmail.com>
2024-04-15 01:25:03 +0530

    app-vercel

--- src/main.ts
+++ src/main.ts
@@ -17,9 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.enableCors({
-    origin:"*"
-  });
+  app.enableCors();
 
   await app.listen(8000);
 }
