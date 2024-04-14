657c2818b7a6d0915261ee74609f09274430afb8
Jayanth MKV <mkvjayanth1@gmail.com>
2024-04-15 01:16:38 +0530

    app

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.enableCors();
+  app.enableCors({
+    origin:"*"
+  });
 
   await app.listen(8000);
 }
