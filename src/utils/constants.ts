051626dc81db5ebaebb3f0819ed56dfba22b590c
Jayanth MKV <mkvjayanth1@gmail.com>
2024-05-09 23:10:24 +0530

    final

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const FRONTEND_URL = "https://mascaa-instructor.vercel.app"
-export const FRONTEND_URLS = "https://mascaa-user.vercel.app"
+export const FRONTEND_URL = "https://mascaa-instructor.eddrix.com"
+export const FRONTEND_URLS = "https://mascaa.eddrix.com"
 // export const FRONTEND_URL = "http://localhost:3000"
 // export const FRONTEND_URLS = "http://localhost:3001"
